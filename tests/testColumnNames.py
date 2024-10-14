92cede72b7c2415284a5e67499faf34c454a363d
pndungutse2 <111838565+pndungutse2@users.noreply.github.com>
2024-10-13 21:48:45 -0500

    truth

--- tests/testColumnNames.py
+++ tests/testColumnNames.py
@@ -22,3 +22,6 @@ class testCases(unittest.TestCase):
       truth = pd.read_csv("tests/files/answer.csv")
 
       self.assertTrue(all(truth.columns[i] == lego2019.columns[i] for i in range(len(truth.columns))), "Your dataset does not contain all of the required columns.")
+
+print(lego2019.columns)
+print(truth.columns)
